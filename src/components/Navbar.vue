a149e05cb1e57b65351706ae227c2b84f3004043
Matěj Jezdinský <matejjezdinsky@gmail.com>
2020-02-18 21:10:50 +0100

    rough skelet of navbar, footer and contact page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="flex flex-row justify-between">
-    <h1>Navbar</h1>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/seller">Seller</router-link> - 
-      <router-link to="/buyer">Buyer</router-link> - 
-      <router-link to="/about">About</router-link> - 
-      <router-link to="/contact">Contact</router-link>
-    </nav>
+  <div class="navbar p-8">
+    <div class="flex flex-row justify-between" style="height: 50px;">
+      <div class="w-full h-full">
+        <router-link to="/">
+          <img src="@/assets/Logo-lowres.png" class="w-auto h-full">
+        </router-link>
+      </div>
+      <nav class="flex flex-row">
+        <router-link to="/seller">Seller</router-link>
+        <router-link to="/buyer">Buyer</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <div style="width: max-content;">
+          <router-link to="/register">Register</router-link> 
+          <router-link to="/signin">Sign&nbsp;in</router-link>
+        </div> 
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -16,3 +25,12 @@ export default {
   name: 'Navbar'
 }
 </script>
+
+<style scoped>
+.router-link-active {
+  text-decoration: underline 2px;
+}
+nav > * {
+  margin: 8px;
+}
+</style>
