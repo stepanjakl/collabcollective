e44950f272eed34de390b07376bf6eac088e4ba6
Matěj Jezdinský <matejjezdinsky@gmail.com>
2020-02-19 16:35:29 +0100

    pages content mock-ups

--- src/views/Buyer.vue
+++ src/views/Buyer.vue
@@ -1,11 +1,68 @@
 <template>
-  <div>
-    <h1>Buyer</h1>
-  </div>
+	<div class="page">
+
+        <div class="top-picture relative z-0">
+            <img src="@/assets/home-lowres.jpg" alt="Hero pisture">
+            <div class="absolute">
+                <h1 class="pb-2">One stop 24/7 to buy the content you need.</h1>
+                <button>Register Now</button>
+            </div>
+        </div>
+
+		<div class="over-picture p-16">
+			<div class="relative p-8 bg-gray-300" style="top: -130px; margin-bottom: -130px;">
+				<h1 class="pb-2">Rights management and marketplace.</h1>
+			<p>Asdgaexvb bdbwrtsdg vae sbb ndgnc tdnrbdzret rstngfb.</p>
+			</div>
+		</div>
+
+		<div class="relative py-8">
+            <section-title title="Key features"></section-title>
+        </div>
+
+		<div class="p-8">
+            <div class="flex xl:flex-row lg:flex-row md:flex-col sm:flex-col flex-col">
+
+                <div class="xl:w-1/2 lg:w-1/2 md:w-full sm:w-full w-full">
+                    <h3 class="pb-2">Nadpis</h3>
+                    <p>ASdasdssadfasgf dfgdfav gr sste n rr etngbwrst dthrgbeu n gb ryjmdn fgwys t hgnryutn dfvf erdh byetn.</p>
+                </div>
+
+                <div class="md:pl-16 pl-0 xl:w-1/2 lg:w-1/2 md:w-full sm:w-full w-full">
+                    <img src="@/assets/aside-lowres.jpg" class="w-full h-full">
+                </div>
+
+            </div>
+        </div>
+		
+	</div>
 </template>
 
 <script>
+import SectionTitle from '../components/SectionTitle';
+
 export default {
-  name: 'Buyer'
+	name: 'Buyer',
+	components: {
+        SectionTitle
+	}
 }
 </script>
+
+<style scoped>
+.top-picture {
+    width:  100%;
+    height: 100vh;
+    position: relative;
+    z-index: 0;
+}
+.top-picture > img {
+    width:  100%;
+    height: 100%;
+}
+.top-picture > div {
+    max-width: 600px;
+    top: 30%;
+    left: 60px;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,54 +1,58 @@
 <template>
-  <div class="contact p-8">
-    <h1>Get in touch with us!</h1>
-    <div class="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col flex-col">
+	<div class="page p-8">
 
-      <div class="relative xl:w-3/4 lg:w-3/4 md:w-3/4 sm:w-full w-full" style="max-width: 500px; margin: 0 auto;">
+		<div class="relative w-full" style="max-height: 100vh">
+			<img src="@/assets/contact-lowres.jpg" style="w-full h-full">
+		</div>
 
-        <form action="sendMail">
-          <div class="flex flex-row">
+		<div class="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col flex-col">
 
-            <div class="w-1/2 flex flex-col">
-              <label>Full Name</label>
-              <input type="text">
-            </div>
-            <div class="w-1/2 flex flex-col">
-              <label>Full Name</label>
-              <input type="text">
-            </div>
-          
-          </div>
+			<div class="relative xl:w-3/4 lg:w-3/4 md:w-3/4 sm:w-full w-full" style="max-width: 500px; margin: 0 auto;">
 
-          <div class="w-full flex flex-col">
-            <label>Full Name</label>
-            <textarea></textarea>
-          </div>
+				<form action="sendMail">
+					<div class="flex flex-row">
 
-          <div class="absolute bottom-0 right-0">
-            <button>Send Message</button>
-          </div>
+						<div class="w-1/2 flex flex-col">
+							<label>Full Name</label>
+							<input type="text">
+						</div>
+						<div class="w-1/2 flex flex-col">
+							<label>Full Name</label>
+							<input type="text">
+						</div>
 
-        </form>
+					</div>
 
-      </div>
-      <div class="md:ml-4 sm:ml-0 xl:w-1/4 lg:w-1/4 md:w-1/4 sm:w-full w-full">
-        <h6>Our Email:</h6>
-        <p>info@contentarena.com</p>
-        <h6>Address:</h6>
-        <p>
-          afdasfafbr<br>
-          afdasfa asdfbr<br>
-          afdasasfafbr<br>
-          afda fafbr<br>
-        </p>
-      </div>
+					<div class="w-full flex flex-col">
+						<label>Full Name</label>
+						<textarea></textarea>
+					</div>
 
-    </div>
-  </div>
+					<div class="absolute bottom-0 right-0">
+						<button>Send Message</button>
+					</div>
+
+				</form>
+
+			</div>
+			<div class="md:ml-4 sm:ml-0 xl:w-1/4 lg:w-1/4 md:w-1/4 sm:w-full w-full">
+				<h6>Our Email:</h6>
+				<p>info@contentarena.com</p>
+				<h6>Address:</h6>
+				<p>
+					afdasfafbr<br>
+					afdasfa asdfbr<br>
+					afdasasfafbr<br>
+					afda fafbr<br>
+				</p>
+			</div>
+
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Contact'
+	name: 'Contact'
 }
 </script>
