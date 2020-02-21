761785d6a495f56b3f936ed043a57bab38467875
Matěj Jezdinský <matejjezdinsky@gmail.com>
2020-02-21 21:17:26 +0100

    page footer upgrade

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -6,65 +6,69 @@
 				<img src="@/assets/Logo-w-lowres.png" class="w-auto mb-2" style="height: 40px">
 			</div>
 
-			<div class="flex flex-wrap">
-			
-				<div class="w-1/2">
-					<h2 class="font-bold text-white text-4xl p-2">Join us today!</h2>
+			<div class="flex sm:flex-row flex-col">
+				<div class="sm:w-1/2 w-full">
+
+					<h2 class="text-bold text-white text-4xl p-2">Join us today!</h2>
+
 					<p class="text-white text-md">
 						Buyers: find interesting rights <br>
 						opportunities &amp; optimize your content <br>
 						portfolio.
 					</p>
 
-					<div class="flex flex-row">
+					<div class="flex flex-row pt-8 pb-8">
 						<div class="w-1/2">
-							<p>Buyers</p>
+							<router-link to="/buyer" class="btn btn-shallow-white">Buyers</router-link>
 						</div>
 						<div class="w-1/2 pl-4">
-							<p>Sellers</p>
+							<router-link to="/seller" class="btn btn-shallow-black">Sellers</router-link>
 						</div>
 					</div>
 
 				</div>
 
-				<div class="w-1/2 flex flex-row">
+				<div class="sm:w-1/2 w-full sm:mt-0 mt-8 flex flex-row">
 				
 					<div class="w-1/2 pl-4">
-						<h3 class="font-bold text-cc-blue-darkest mb-4" style="font-variant: small-caps;">Info</h3>
+						<h3 class="text-bolder text-cc-blue-darkest mb-4" style="font-variant: small-caps;">Info</h3>
 						<ul>
-							<li class="font-medium text-white text-lg mb-3">FAQ</li>
-							<li class="font-medium text-white text-lg mb-3">Contact</li>
-							<li class="font-medium text-white text-lg mb-3">Cookies Policy</li>
+							<li class="text-medium text-white text-lg mb-3">FAQ</li>
+							<li class="text-medium text-white text-lg mb-3">Contact</li>
+							<li class="text-medium text-white text-lg mb-3">Cookies Policy</li>
 						</ul>
 					</div>
 
 					<div class="w-1/2 pl-4">
-						<h3 class="font-bold text-cc-blue-darkest mb-4" style="font-variant: small-caps;">Policy</h3>
+						<h3 class="text-bolder text-cc-blue-darkest mb-4" style="font-variant: small-caps;">Policy</h3>
 						<ul>
-							<li class="font-medium text-white text-lg mb-3">Terms</li>
-							<li class="font-medium text-white text-lg mb-3">Privacy</li>
+							<li class="text-medium text-white text-lg mb-3">Terms</li>
+							<li class="text-medium text-white text-lg mb-3">Privacy</li>
 						</ul>
 					</div>
 
 				</div>
-
 			</div>
 
-			<div class="flex flex-wrap mt-4">
-				<div class="w-1/2">
+			<div class="flex flex-wrap sm:flex-row flex-col sm:mt-4 mt-8">
+				<div class="sm:w-1/2 w-full">
 					<button class="btn btn-yellow">Register Now</button>
 				</div>
-				<div class="w-1/2 pl-4">
-					<p>Facebook - Linked In</p>
+				<div class="sm:w-1/2 w-full pl-4 pt-2">
+					<p class="text-white text-lg inline">
+						<span class="h-2px mr-4 mb-1 bg-white inline-block" style="width: 24px"></span>
+						<a href="" class="mr-4">Facebook</a>
+						<a href="">Linked In</a>
+					</p>
 				</div>
 			</div>
 
 			<div class="flex flex-wrap mt-16">
 				<div class="w-1/2">
-					<p class="text-sm text-cc-blue-darkest">&copy; Content Arena</p>
+					<p class="text-cc-blue-darkest text-sm">&copy; Content Arena</p>
 				</div>
 				<div class="w-1/2 pl-4">
-					<p class="text-sm text-cc-blue-darkest">All rights reserved</p>
+					<p class="text-cc-blue-darkest text-sm">All rights reserved</p>
 				</div>
 			</div>
 
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,53 +1,57 @@
 <template>
-	<div class="page p-8">
+	<div class="page">
 
-		<div class="container mx-auto">
-			<div class="relative w-full pt-20 pb-16 text-center bg-gray-100">
+		<div class="w-full bg-gray-100">
+			<div class="container mx-auto">
+				<div class="relative w-full pt-20 pb-16 text-center">
 
-				<h1 class="text-bolder text-cc-blue-darkest text-4xl">Our Locations</h1>
-				<img src="@/assets/contact-lowres.jpg" class="mx-auto">
-				<p class="text-normal text-cc-blue-light text-md uppercase">Argentina (Americas) * United Kingdom, France, Germany, &amp; Spain (Europe) * South Africa (Africa) * Singapore (Asia)</p>
+					<h1 class="text-bolder text-cc-blue-darkest text-4xl">Our Locations</h1>
+					<img src="@/assets/contact-lowres.jpg" class="mx-auto">
+					<p class="text-normal text-cc-blue-light text-md uppercase">Argentina (Americas) * United Kingdom, France, Germany, &amp; Spain (Europe) * South Africa (Africa) * Singapore (Asia)</p>
 
+				</div>
 			</div>
 		</div>
 
 		<div class="container mx-auto p-8">
-			<div class="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col flex-col">
 
-				<h2 class="text-bolder text-cc-blue-darkest text-3xl">Get in touch with us</h2>
+			<h2 class="text-bold text-cc-blue-darkest text-3xl pb-8">Get in touch with us</h2>
 
-				<div class="relative xl:w-3/4 lg:w-3/4 md:w-3/4 sm:w-full w-full" style="max-width: 500px; margin: 0 auto;">
+			<div class="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col flex-col  mb-20">
+				<div class="relative xl:w-3/4 lg:w-3/4 md:w-3/4 sm:w-full w-full mx-auto pr-16">
 
 					<form action="sendMail">
 						<div class="flex flex-row">
 
-							<div class="w-1/2 flex flex-col">
-								<label>Full Name</label>
-								<input type="text">
+							<div class="mr-4 w-1/2 flex flex-col">
+								<label class="text-extrabold text-black text-sm mb-4">Full Name</label>
+								<input class="outline" type="text" placeholder="Write...">
 							</div>
-							<div class="w-1/2 flex flex-col">
-								<label>Title</label>
-								<input type="text">
+							<div class="ml-4 w-1/2 flex flex-col">
+								<label class="text-extrabold text-black text-sm mb-4">Title</label>
+								<input class="outline" type="text" placeholder="Write...">
 							</div>
 
 						</div>
 
-						<div class="w-full flex flex-col">
-							<label>Text</label>
-							<textarea></textarea>
+						<div class="w-full flex flex-col mt-8">
+							<label class="text-extrabold text-black text-sm mb-4">Title</label>
+							<textarea class="outline" rows="6" placeholder="Write..." style="width: calc(100% + 7px)"></textarea>
 						</div>
 
-						<div class="absolute bottom-0 right-0">
-							<button>Send Message</button>
+						<div class="flex flex-row justify-end relative" style="top: 32px; right: -7px;">
+							<button class="btn btn-black rounded-none">Send Message</button>
 						</div>
 
 					</form>
 
 				</div>
 				<div class="md:ml-4 sm:ml-0 xl:w-1/4 lg:w-1/4 md:w-1/4 sm:w-full w-full">
-					<h6>Our Email:</h6>
+					
+					<h6 class="text-extrabold text-gray-800 text-sm mb-2">Our Email:</h6>
 					<p>info@contentarena.com</p>
-					<h6>Address:</h6>
+					
+					<h6 class="text-extrabold text-gray-800 text-sm mb-2 mt-8">Address:</h6>
 					<p>
 						afdasfafbr<br>
 						afdasfa asdfbr<br>
@@ -58,6 +62,7 @@
 
 			</div>
 		</div>
+
 	</div>
 </template>
 
@@ -68,5 +73,8 @@
 </script>
 
 <style scoped>
-
+.outline {
+	padding: 8px;
+	outline: solid lightgray 1px;
+}
 </style>
