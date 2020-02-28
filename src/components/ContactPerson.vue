2114093d5c65cc94945c93490d04a541be3fc452
Stepan Jakl <stevie.jakl@gmail.com>
2020-02-28 14:05:48 +0000

    Update 'Contact' page UI and content

---
+++ src/components/ContactPerson.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="md:w-1/4 w-1/2 xl:px-8 px-4 mb-16 text-center">
+        <img alt="Sascha Kojic" :src="require('@/assets/people/'+ this.photo +'.jpg')" class="py-2 mx-auto w-40">
+        <h3 class="text-lg text-gray-700 font-semibold uppercase pb-1 pt-2">{{this.name}}</h3>
+        <p class="text-md font-light text-gray-600">{{this.role}}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ContactPerson',
+        props: {
+            'name': String,
+            'role': String
+        },
+        computed: {
+            photo() {
+                return this.name.split(' ').join('-').toLocaleLowerCase()
+            }
+        }
+    }
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,32 @@
 <template>
-	<div class="page">
+    <div class="page">
 
         <top-picture
-            title="We make sports media management easy."
-            comment="Bringing efficiency to the business">
+                title="We make sports media management easy."
+                comment="Bringing efficiency to the business">
             <v-lazy-image class="w-full h-full absolute object-cover top-0 no-blur"
                           :src="require('@/assets/headers/about.jpg')"
                           :src-placeholder="require('@/assets/headers/about-lowres.jpg')"/>
         </top-picture>
 
-        <div class="bg-gray-300">
+        <div class="bg-gray-200">
 
             <div class="pt-4"></div>
             <div class="relative py-8">
                 <section-title title="About us"></section-title>
             </div>
 
-            <div class="container mx-auto md:p-20 p-8 pb-16 pt-0">
+            <div class="container mx-auto md:pl-20 pt-12 p-8 pb-24 pt-0">
 
-                <h2 class="text-bolder text-4xl pb-4">Our story</h2>
+                <h2 class="font-semibold text-4xl pb-6 text-gray-800">Our story</h2>
 
-                <div class="flex md:flex-row flex-col justify-around">
-                    <div class="md:pr-4 pr-0 md:w-1/2 w-full">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat id dolor sed pharetra. Donec consectetur dolor a lectus condimentum malesuada. Nam facilisis nisi eget justo mollis, vitae viverra ex rutrum. Morbi dolor augue, vestibulum id posuere in, lacinia ut nibh. Quisque in neque pretium, egestas tellus eu.</p>
-                    </div>
-                    <div class="md:pl-4 pl-0 md:w-1/2 w-full">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat id dolor sed pharetra. Donec consectetur dolor a lectus condimentum malesuada. Nam facilisis nisi eget justo mollis, vitae viverra ex rutrum. Morbi dolor augue, vestibulum id posuere in, lacinia ut nibh. Quisque in neque pretium, egestas tellus eu.</p>
-                    </div>
+                <div class="text-justify leading-loose text-gray-600" style="columns: 2; column-gap: 4rem;">
+                    Content Arena was founded by a team of sports media rights experts with the vision to provide
+                    bespoke solutions that support sports media rights sellers and buyers in optimising their
+                    commercial business processes. Content Arena’s software solutions aim to improve the efficiency,
+                    transparency, and simplicity of complex processes in an ever-growing
+                    industry which continues to become more fragmented with an increasing amount of available
+                    content as well as a growing number for media companies with content needs.
                 </div>
 
             </div>
@@ -39,29 +39,13 @@
 
         <div class="container mx-auto flex flex-row md:justify-between sm:justify-center justify-center flex-wrap sm:p-8 p-2">
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Erik Lorenz" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Erik Lorenz</h3>
-                <p class="text-md text-gray-700">CEO</p>
-            </div>
+            <ContactPerson name="Erik Lorenz" role="CEO"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Chuan Talco Cruz" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Chuan Talco Cruz</h3>
-                <p class="text-md text-gray-700">CTO</p>
-            </div>
+            <ContactPerson name="Aslinda Ali" role="CTO"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Aslinda Ali" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Aslinda Ali</h3>
-                <p class="text-md text-gray-700">Cotnent Activation Manager</p>
-            </div>
+            <ContactPerson name="Jamie Rascon" role="Content Activation Manager"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Jamie Rascon" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Jamie Rascon</h3>
-                <p class="text-md text-gray-700">Bussiness Developer</p>
-            </div>
+            <ContactPerson name="Juan Talco Cruz" role="Business Developer"/>
 
         </div>
 
@@ -71,54 +55,33 @@
 
         <div class="container mx-auto flex flex-row md:justify-between sm:justify-center justify-center flex-wrap sm:p-8 p-2">
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Sascha Kojic" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Sascha Kojic</h3>
-                <p class="text-md text-gray-700">Co-Founder Content Arena <br/> &amp; CEO SN1 Consulting</p>
-            </div>
+            <ContactPerson name="Sascha Kojic" role="Co-Founder Content Arena & CEO SN1 Consulting"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Bernhard Reidlsperger" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Bernhard Reidlsperger</h3>
-                <p class="text-md text-gray-700">Co-Founder Content Arena <br/> &amp; President Hogmore Media AG</p>
-            </div>
+            <ContactPerson name="Bernhard Reidlsperger" role="Co-Founder Content Arena & President Hogmore Media AG"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Klemens Kögle" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Klemens Kögle</h3>
-                <p class="text-md text-gray-700">Co-Founder Content Arena <br/> &amp; CEO Hogmore Media AG</p>
-            </div>
+            <ContactPerson name="Klemens Kögle" role="Co-Founder Content Arena & CEO Hogmore Media AG"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="John Gleasure" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">John Gleasure</h3>
-                <p class="text-md text-gray-700">Chief Bussiness Developer <br/> Officer DANZ Media</p>
-            </div>
+            <ContactPerson name="John Gleasure" role="Chief Bussiness Developer Officer DANZ Media"/>
 
-            <div class="md:w-auto w-1/2 xl:px-8 px-4 mb-4 text-center">
-                <img alt="Alexander Fryba" src="@/assets/people-lowres.jpg" class="py-2 mx-auto">
-                <h3 class="text-xl text-gray-800 capitalize pb-1">Alexander Fryba</h3>
-                <p class="text-md text-gray-700">Product Developer <br/> Expert</p>
-            </div>
+            <ContactPerson name="Alexander Fryba" role="Product Developer Expert"/>
 
         </div>
 
-	</div>
+    </div>
 </template>
 
 <script>
     import SectionTitle from '../components/SectionTitle';
     import TopPicture from '../layout/TopPicture';
+    import ContactPerson from "@/components/ContactPerson";
 
     export default {
         name: 'Buyer',
         components: {
+            ContactPerson,
             SectionTitle,
             TopPicture
-        }
+        },
     }
 </script>
 
-<style scoped>
-
-</style>
